cb6a382d9982c42bce56caef15ec3e13f122a1ed
vishvesh14 <vishveshsavant@gmail.com>
2018-08-27 17:50:22 +0530

    First Commit with files

---
+++ src/testBase/BasicAuth.java
@@ -0,0 +1,31 @@
+package testBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BasicAuth {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public String chromeDriverPath = "D:/Automation Tools/Chrome_Driver/chromedriver_win32/chromedriver.exe";
+    public String url = "http://the-internet.herokuapp.com/";
+
+    @BeforeClass
+    public void setup(){
+        System.out.println("Launching Chrome Browser");
+        System.setProperty("webdriver.chrome.driver",chromeDriverPath);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
+    @AfterClass
+    public void exit(){
+        System.out.println("Test Complete");
+    }
+
+}
